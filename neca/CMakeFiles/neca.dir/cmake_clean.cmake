e8dba27c1acee79327ded46935f8fcd1ea498d1a
Lucas Nathaniel <lucasnataniel@hotmail.com>
2018-04-18 00:54:15 -0400

    rm module neca but add :)

---
+++ neca/CMakeFiles/neca.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/neca.dir/src/main.cpp.o"
+  "neca.pdb"
+  "neca"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/neca.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
